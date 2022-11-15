b40fbaec068ec1b4815170403d08d0859ad78e16
Dong <Dong@DESKTOP-T3PVJ4G>
2022-11-16 00:47:30 +0900

    초기 커밋.

---
+++ AUTHORS.md
@@ -0,0 +1,21 @@
+# Intersect Authors
+
+[JC Snider](https://github.com/jcsnider)
+
+[Joe Bridges](https://github.com/irokaiser)
+
+[Robbie Lodico](https://github.com/lodicolo)
+
+# Intersect Maintainers
+
+[Jocelyn Cobb](https://github.com/Cheshire92)
+
+# Intersect Contributors
+
+[Alex Vild](https://github.com/AlexVild)
+
+[A. I. Robin](https://github.com/alloin)
+
+[Dog from the Kash Shop](#)
+
+[Fernando Arzola Lagos](https://github.com/Arufonsu)
---
+++ Intersect.Client/Core/Controls/ControlEnum.cs
@@ -0,0 +1,90 @@
+﻿namespace Intersect.Client.Core.Controls
+{
+
+    public enum Control
+    {
+
+        MoveUp,
+
+        MoveLeft,
+
+        MoveDown,
+
+        MoveRight,
+
+        AttackInteract,
+
+        Block,
+
+        AutoTarget,
+
+        PickUp,
+
+        Enter,
+
+        Hotkey1,
+
+        Hotkey2,
+
+        Hotkey3,
+
+        Hotkey4,
+
+        Hotkey5,
+
+        Hotkey6,
+
+        Hotkey7,
+
+        Hotkey8,
+
+        Hotkey9,
+
+        Hotkey0,
+
+        Screenshot,
+
+        OpenMenu,
+
+        OpenInventory,
+
+        OpenQuests,
+
+        OpenCharacterInfo,
+
+        OpenParties,
+
+        OpenSpells,
+
+        OpenFriends,
+
+        OpenSettings,
+
+        OpenDebugger,
+
+        OpenAdminPanel,
+
+        ToggleGui,
+
+        Hotkey10,
+
+        Hotkey11,
+
+        Hotkey12,
+
+        Hotkey13,
+
+        Hotkey14,
+
+        Hotkey15,
+
+        Hotkey16,
+
+        Hotkey17,
+
+        Hotkey18,
+
+        Hotkey19
+    }
+
+}
